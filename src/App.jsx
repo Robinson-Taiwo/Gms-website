523cf8629708ab3146092be3a34b91c21183318b
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2023-03-27 22:04:10 +0100

    feature:newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,30 @@ import Strength from "./Strength";
 import school from "../src/assets/school1.jpeg"
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-AOS.init()
 import "./App.css"
 
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 1000, // values from 0 to 3000, with step 50ms
+  duration: 2000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
 
 
 const App = () => {
@@ -33,15 +54,15 @@ const App = () => {
   // changing nav colour when scrolling
   const [colour, setColour] = useState(false)
 
-  const changeColour = () => {
-    if (window.scrollY >= 10) {
-      setColour(true)
-    } else {
-      setColour(false)
-    }
-  }
+  // const changeColour = () => {
+  //   if (window.scrollY >= 10) {
+  //     setColour(true)
+  //   } else {
+  //     setColour(false)
+  //   }
+  // }
 
-  window.addEventListener("scroll", changeColour)
+  // window.addEventListener("scroll", changeColour)
 
   const kindClick = () => {
     setHeading("kindness")
@@ -57,8 +78,11 @@ const App = () => {
 
   return (
     <div className="home">
-      <nav className={colour ? "navbar-active" : "navbar"}>
+      <nav className="navbar">
         <img src={logo1} alt="" className="logo" />
+        <div className="login">
+          <a href="" className="href">Login</a>
+        </div>
       </nav>
 
       <div className="slider">
@@ -171,8 +195,15 @@ const App = () => {
         <div className="second-2">
 
           <div className="school-sect">
-            <h1 className="the-school">
-              About Our School
+            <h1 data-aos="fade-down" data-aos-offset="200"
+              // data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out"
+              data-aos-mirror="true"
+              // data-aos-once="true"
+              data-aos-anchor-placement="top-center"
+              className="the-school">
+              ABOUT OUR SCHOOL
             </h1>
           </div>
           {/* data-aos="zoom-out-down" data-aos-delay="50"
@@ -184,15 +215,42 @@ const App = () => {
 
           <div className="about-body">
             <div className="body-text">
-              <div className="body-p">
+              <div
+                data-aos="fade-up"
+                data-aos-offset="200"
+                data-aos-delay="50"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                // data-aos-once="false"
+                data-aos-anchor-placement="top-center"
+                className="body-p">
                 Great Marble Group of School is a leading institution of learning that values pride, strength, and kindness. The school prides itself on providing top-quality education to both primary and secondary school students. The institution's mission is to inspire and empower students to achieve their full potential through the provision of an environment that nurtures academic excellence, character development, and lifelong learning.
               </div>
-              <div className="body-p">
+              <div
+                data-aos="fade-up"
+                data-aos-offset="200"
+                data-aos-delay="50"
+                data-aos-duration="2000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                // data-aos-once="false"
+                data-aos-anchor-placement="top-center"
+                className="body-p">
                 Located at 1, Olabanwo Street, off Ofin Road, Igbagbo in Baiyeku LCDA, the Great Marble Group of School is building a reputation for excellence in academics and character development. The school's dedicated and experienced faculty members work tirelessly to provide students with an education that is both rigorous and supportive. Great Marble Group of School is committed to creating a learning environment that is inclusive and values-driven, where students are encouraged to learn and grow.
 
 
               </div>
-              <div className="body-p">
+              <div
+                data-aos="fade-up"
+                data-aos-offset="200"
+                data-aos-delay="50"
+                data-aos-duration="3000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                // data-aos-once="false"
+                data-aos-anchor-placement="top-center"
+                className="body-p">
                 Whether you are a primary school student or a secondary school student, Great Marble Group of School has everything you need to succeed. With a strong focus on academic excellence, character development, and community involvement, this institution is the perfect place to begin your journey towards a bright and successful future. So come and join us at Great Marble Group of School, where education and values go hand in hand!
 
 
@@ -211,21 +269,38 @@ const App = () => {
           <div className="third">
             <h1 data-aos="flip-left"
               data-aos-easing="ease-out-cubic"
-              data-aos-duration="2000" className="mission">
+              data-aos-offset="200"
+              data-aos-delay="50"
+              data-aos-duration="2000"
+              data-aos-mirror="true"
+              // data-aos-once="false"
+              data-aos-anchor-placement="top-center" className="mission">
               OUR MISSION
             </h1>
             <div className="mission-text">
-              <p className="mi-text1"> With the support of the parents and our school community, we ensure</p>
+              <p
+                data-aos="zoom-out"
+                data-aos-offset="200"
+                data-aos-delay="30"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                // data-aos-once="false"
+                data-aos-anchor-placement="top-center"
+                className="mi-text1"> With the support of the parents and our school community, we ensure</p>
 
               <div className="mi-cover">
                 <img src={pin} alt="" className="pin" />
 
-                <p data-aos="fade-up"
-                  data-aos-anchor="#example-anchor"
-                  data-aos-offset="500"
+                <p
+                  data-aos="zoom-in"
+                  data-aos-offset="200"
                   data-aos-delay="50"
-                  data-aos-duration="2000"
-                  data-aos-easing="ease-in-out" className="mi-text2">
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  // data-aos-once="false"
+                  data-aos-anchor-placement="top-center" className="mi-text2">
                   We: Develop self-motivated learners who demonstrate resilience and pride in everything they do.
                 </p>
 
@@ -234,12 +309,14 @@ const App = () => {
               <div className="mi-cover">
                 <img src={pin} alt="" className="pin" />
 
-                <p data-aos="fade-up"
-                  data-aos-anchor="#example-anchor"
-                  data-aos-offset="500"
-                  data-aos-delay="60"
-                  data-aos-duration="2000"
-                  data-aos-easing="ease-in-out" className="mi-text3">
+                <p data-aos="zoom-in"
+                  data-aos-offset="200"
+                  data-aos-delay="70"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  // data-aos-once="false"
+                  data-aos-anchor-placement="top-center" className="mi-text3">
 
                   We: Foster critical thinkers through rich,tailored, and exciting learning opportunities.
                 </p>
@@ -249,12 +326,14 @@ const App = () => {
               <div className="mi-cover">
                 <img src={pin} alt="" className="pin" />
 
-                <p data-aos="fade-up"
-                  data-aos-anchor="#example-anchor"
-                  data-aos-offset="500"
-                  data-aos-delay="70"
-                  data-aos-duration="2000"
-                  data-aos-easing="ease-in-out" className="mi-text4">
+                <p data-aos="zoom-in"
+                  data-aos-offset="200"
+                  data-aos-delay="90"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  // data-aos-once="false"
+                  data-aos-anchor-placement="top-center" className="mi-text4">
 
                   We: Nurture our global citizens to be socially responsible.
                 </p>
@@ -263,12 +342,14 @@ const App = () => {
               <div className="mi-cover">
                 <img src={pin} alt="" className="pin" />
 
-                <p data-aos="fade-up"
-                  data-aos-anchor="#example-anchor"
-                  data-aos-offset="500"
+                <p data-aos="zoom-in"
+                  data-aos-offset="200"
                   data-aos-delay="110"
-                  data-aos-duration="2000"
-                  data-aos-easing="ease-in-out" className="mi-text5">
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  // data-aos-once="false"
+                  data-aos-anchor-placement="top-center" className="mi-text5">
 
                   We: Promote the importance of mutual respect,positive well-being, and kindness.
                 </p>
@@ -277,7 +358,16 @@ const App = () => {
 
               <div className="mi-coverl">
 
-                <p className="mi-text6">
+                <p
+                  data-aos="zoom-out"
+                  data-aos-offset="200"
+                  data-aos-delay="130"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  // data-aos-once="false"
+                  data-aos-anchor-placement="top-center"
+                  className="mi-text6">
                   At Great Marble, our rigorous and forward-thinking  approach to teaching ensures learners have the tools needed to continue on their education path
 
                 </p>
@@ -316,9 +406,15 @@ const App = () => {
 
 
             <div className="footer-icon">
-              <img data-aos="fade-up" data-aos-delay="50"
-                data-aos-duration="2000"
-                data-aos-easing="ease-in-out" src={logo1} alt="" className="icons" />
+              <div className="newsletter">
+                <h1 className="news-head">
+                  Suscribe to our  newsletter to get latest update
+                </h1>
+                <input placeholder="input your email here" type="text" className="news-input" />
+                <button className="submit">
+                  submit
+                </button>
+              </div>
             </div>
 
             <div className="footer-info">
